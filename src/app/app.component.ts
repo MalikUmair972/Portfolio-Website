e31c32c1b24b60f1d40cc76e48804cea32c66beb
MalikUmair972 <mu501362@gmail.com>
2024-10-19 18:08:42 +0500

    About page done!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component'
+import { ContactComponent } from './contact/contact.component'
+import { PortfolioComponent } from './portfolio/portfolio.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    AboutComponent,
+    ContactComponent,
+    PortfolioComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
