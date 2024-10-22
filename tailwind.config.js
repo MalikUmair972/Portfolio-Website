2d07a4588aa818f18ae62d15cee960282e77f870
MalikUmair972 <mu501362@gmail.com>
2024-10-22 17:00:04 +0500

    Upgrade Functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'white': '10px 04px 30px rgba(255, 255, 255, 0.5)',
+      },
+    },
   },
   plugins: [],
 }
