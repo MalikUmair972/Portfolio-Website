dda491cbd17ce18f1d6405776e14d6fb12a2ed98
MalikUmair972 <mu501362@gmail.com>
2024-11-22 16:56:21 +0500

    Add Form Functionality

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl,ReactiveFormsModule,FormBuilder, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm = new FormGroup ({
+   name: new FormControl(''),
+   email: new FormControl(''),
+   number: new FormControl(''),
+   subject: new FormControl(''),
+   message: new FormControl('')
 
+  //  if (FormControl)
+  });
+
+  contactUser() {
+    console.warn(this.contactForm.value)
+  }
 }
