7cb9e1f5852faf2cb7928626d5cc7b40a1af4e87
MalikUmair972 <mu501362@gmail.com>
2025-01-28 16:09:25 +0500

    Making Object

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+
+  skills = [
+    {
+      Image: 'assets/js.png',
+      name: 'Javascript',
+      Percent: '75%'
+    },
+    {
+      Image: 'assets/angular.png',
+      name: 'Angular',
+      Percent: '88%'
+    },
+    {
+      Image: 'assets/tailwind.png',
+      name: 'Tailwind Css',
+      Percent: '98%'
+    },
+    {
+      Image: 'assets/webflow.png',
+      name: 'Webflow',
+      Percent: '85%'
+    },
+    {
+      Image: 'assets/photoshop.png',
+      name: 'Adobe Photoshop',
+      Percent: '97%'
+    },
+    {
+      Image: 'assets/typescript.png',
+      name: 'Typescript',
+      Percent: '78%'
+    },
+  ]
+    
+  
 
 }
